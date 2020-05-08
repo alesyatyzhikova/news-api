2ccffdd3a07f7d42940934b3d5843b8c8ce54480
alesya_tyzhikova <a.tuzhikova@skyway.capital>
2020-05-08 18:53:45 +0300

    cors

--- middlewars/auth.js
+++ middlewars/auth.js
@@ -4,12 +4,12 @@ const { JWT_SECRET } = require('../config');
 const NotAuthError = require('../errors/notAuthError');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.header;
+  const { authorization } = req.headers;
 
   if (!authorization) throw new NotAuthError();
 
+  const token = authorization.replace('Bearer ', '');
   let payload;
-  const token = authorization.split(' ')[1];
 
   try {
     payload = jwt.verify(token, JWT_SECRET);
