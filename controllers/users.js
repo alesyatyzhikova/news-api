5de4b9ed0f3f3f4a9a083890ea4ad75bcababccf
alesya_tyzhikova <a.tuzhikova@skyway.capital>
2020-05-10 13:17:02 +0300

    email model

--- controllers/users.js
+++ controllers/users.js
@@ -38,7 +38,7 @@ module.exports.createUser = (req, res, next) => {
 // Аутентификация
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
-  User.findUserByCredentials(email, password)
+  return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
       res.cookie('jwt', token, {
