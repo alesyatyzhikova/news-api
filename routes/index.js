9ef2db9afdb06ccdffb8a326cb8732f4822faf7a
alesya_tyzhikova <a.tuzhikova@skyway.capital>
2020-05-14 15:18:05 +0300

    cors

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,8 @@ const NotFoundError = require('../errors/notFoundError');
 router.use(cookieParser());
 
 router.use(cors({
-  origin: 'https://alesyatyzhikova.github.io',
+  origin: 'http://localhost:8080',
+  // origin: 'https://alesyatyzhikova.github.io',
   credentials: true,
 }));
 
